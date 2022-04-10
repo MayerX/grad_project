149d8d1c9cf8e8aa2138b1c3e8f10943058b6b13
MayerX <meet_fate_x@icloud.com>
2022-04-10 16:01:21 +0800

    start crawl questions and answers

--- crwal/pre_question_spider.py
+++ crwal/pre_question_spider.py
@@ -1,43 +1,52 @@
 import time
-import requests
-import lxml
-import selenium
+
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 
 
 def pre_question_spider(province_id):
+    # 浏览器设置
     driver = webdriver.Chrome()
     driver.set_page_load_timeout(10)
     driver.set_script_timeout(10)
+    # 超出时间则停止加载
     try:
         driver.get('https://gaokao.chsi.com.cn/zxdy/#zx')
     except:
         driver.execute_script('window.stop()')
     forumid = []
+    universitie_names = []
+    # 找到选择的省份链接，广东省是44
     pro = driver.find_element(by=By.XPATH, value='//a[@name={}]'.format(province_id))
+    # 模拟移动光标，并点击按钮进入省份所在的所有大学
     ActionChains(driver).move_to_element(driver.find_element_by_id('yxszdSelector')).perform()
     ActionChains(driver).click(pro).perform()
+    # 为防止没有加载完成设置挂载时间
     time.sleep(3)
-    # next_page = driver.find_elements(by=By.XPATH, value="//li[@class='lip']")
+    # 用bs4解析网页信息
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    max_page = int(soup.find_all('li')[-2].text[0])
-    # print(soup.find_all('li')[-3])
+    # 设置该省份所在大学数的最大页数
+    # max_page = int(soup.find_all('li')[-2].text[0])
+    max_page = 1
+    # 读取该省份所有大学在该网页的编码
     for page in range(max_page):
         time.sleep(3)
         soup = BeautifulSoup(driver.page_source, 'lxml')
         items = soup.find_all('div', class_="yx-item")
         for item in items:
             forumid.append(item.find_all('a')[0]['href'].split(',')[1])
+            universitie_names.append(item.find('span', class_='yxmc-span').text)
         next_page = driver.find_element(by=By.LINK_TEXT, value='下一页')
         if next_page is not None:
             ActionChains(driver).click().perform()
-        # forumid.append(soup.find_all('div', class_="yx-item")[0].find_all('a')[0]['href'].split(',')[1])
-    # for id in forumid:
-    #     print(id)
-    return forumid
+    with open('data/universities_code.txt', 'w') as file:
+        for index in range(universitie_names.__len__()):
+            file.write(universitie_names[index] + ':' + str(forumid[index]) + '\n')
+            print(universitie_names[index] + ':' + str(forumid[index]))
+        file.close()
+    return universitie_names, forumid
 
 
 if __name__ == '__main__':
--- crwal/question_spider.py
+++ crwal/question_spider.py
@@ -1,10 +1,78 @@
+import csv
+import time
+
 import openpyexcel
 import requests
 from bs4 import BeautifulSoup
+
 from pre_question_spider import pre_question_spider
 
 
-def spider(province_id):
+def spider(universities_code_path):
+    universities_code = []
+
+    # 读取各大学编号
+    with open(universities_code_path, 'r') as file:
+        universities_code = file.readlines()
+        file.close()
+
+    # 对各大学数据爬取前进行预处理
+    for item in universities_code:
+        universities_name, forumid = item.split(':')
+        # print('name: ' + universities_name)
+        # print('code: ' + forumid[:-1])
+        print('------------------------')
+        print('开始爬取{}的招生问题'.format(universities_name))
+        print('------------------------')
+        file_path = 'questions/{}.csv'.format(universities_name)
+        spider_to_csv(file_path, forumid[:-1])
+        time.sleep(10)
+    pass
+
+
+def spider_to_csv(file_path, code):
+    # print(file_path + ': ' + code)
+    with open(file_path, 'wb') as file:
+        # 设置csv写入变量
+        writer = csv.writer(file)
+        # 设置csv文件标题头
+        writer.writerow(['ID', '问题', '答案'])
+        # 获取最大页数
+        response = requests.get(
+            'https://gaokao.chsi.com.cn/zxdy/forum--method-listDefault,{},year-2005,start-{}.dhtml'.format(
+                code, 0))
+        # 解析网页信息
+        soup = BeautifulSoup(response.content, 'lxml')
+        numbers = soup.find_all('li', attrs={"class": "lip"})
+        max_page = int(numbers[-3].text) * 15
+        # 设置ID
+        index = 0
+        # 进行数据爬取
+        for page in range(0, max_page, 15):
+            print('------------------------')
+            print("当前页面: ", page / 15)
+            print('------------------------')
+            response = requests.get(
+                'https://gaokao.chsi.com.cn/zxdy/forum--method-listDefault,{},year-2005,start-{}.dhtml'.format(
+                    code, page))
+            soup = BeautifulSoup(response.content, 'lxml')
+            # 找到该页面所有问题和答案
+            questions = soup.find_all('div', attrs={"class": "question"})
+            answers = soup.find_all('div', attrs={"class": "question_a"})
+            if page != max_page - 15:
+                questions = questions[5:]
+                answers = answers[5:]
+            for question, answer in zip(questions, answers):
+                question = question.text.replace(" ", "").replace("\r\n", "").replace("\n", "")
+                answer = answer.text.replace("\r\n", "").replace("\n", "").replace(" ", "")[5:]
+                print('ID: ' + index.__str__() + ' - ''question: ', question, ' - ', 'answer: ', answer)
+                writer.writerow([index, question, answer])
+                index += 1
+        file.close()
+    pass
+
+
+def spider_to_excel(province_id):
     page = 0
     max_page = 0
     print("创建Excel")
@@ -51,4 +119,5 @@ def spider(province_id):
 
 
 if __name__ == "__main__":
-    spider(44)
+    universities_code_path = 'data/universities_code.txt'
+    spider(universities_code_path)
